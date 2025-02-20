6dbc0dd302dfd163d6d515a0bf7379f1f0d9e0a9
Muhammadamin <abdullayevmuhammadamin89@example.com>
2025-02-20 19:09:44 +0500

    firstCommit

---
+++ src/main/java/org/example/deliveryservicebotpro/DeliveryServiceBotProApplication.java
@@ -0,0 +1,13 @@
+package org.example.deliveryservicebotpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceBotProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryServiceBotProApplication.class, args);
+    }
+
+}
