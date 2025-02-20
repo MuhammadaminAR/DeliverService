2efbc2a3f443901514384882495adf27c35b4d6e
Muhammadamin <abdullayevmuhammadamin89@example.com>
2025-02-20 19:59:11 +0500

    dependency

---
+++ src/main/java/org/example/deliveryservicebotpro/entity/User.java
@@ -0,0 +1,19 @@
+package org.example.deliveryservicebotpro.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+}
